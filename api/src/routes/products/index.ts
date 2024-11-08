df1d52626738f8657bbf05c91cb9f3d2f8fbc526
Roman Organ <r.organ@dan-it.com.ua>
2024-11-08 14:24:53 +0200

    zod and drizzle schema validation

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -6,6 +6,11 @@ import {
   updateProduct,
   deleteProduct,
 } from "./productsController";
+import { validateData } from "../../middlewares/validationMiddleware";
+import {
+  createProductSchema,
+  updateProductSchema,
+} from "../../db/productsSchema";
 
 const router = Router();
 
@@ -13,9 +18,9 @@ router.get("/", listProducts);
 
 router.get("/:id", getProductById);
 
-router.post("/", createProduct);
+router.post("/", validateData(createProductSchema), createProduct);
 
-router.put("/:id", updateProduct);
+router.put("/:id", validateData(updateProductSchema), updateProduct);
 
 router.delete("/:id", deleteProduct);
 
