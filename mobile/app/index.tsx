e52d197cb8d1bfb3d432b2919551da2435dd5661
Roman Organ <r.organ@dan-it.com.ua>
2024-11-12 17:03:40 +0200

    setup expo router

---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 40 }}>Home Screen</Text>
+    </View>
+  );
+}
